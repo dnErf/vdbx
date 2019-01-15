11712ee4b198dc06653d1c27392e3d433be748b5
dnErf <frend.fernando@live.com>
2019-01-15 07:53:07 -0800

    add lint and some fixes

--- lens.js
+++ lens.js
@@ -1,22 +1,25 @@
-;(function(){
+;(function() {
 
-  // var l;
-  
-  function Lob (lthru,obj) {
-    
+  // var lob
+
+  function Lob(lthru, obj) {
+
     const { prototype } = Lob
 
     lthru = lthru.toString().split('/').slice(1).slice(0, -1)
 
-    var 
-      mkeys = []
+    var
+      lob = this.lob
+      , mkeys = []
       , rdx = obj
 
-    function see(get,subj) { return get(subj) }
+    function see(get, subj) {
+      return get(subj)
+    }
 
     function nGet(lenspath) {
       return (subj) => {
-        lenspath.forEach((_,index) => {
+        lenspath.forEach((_, index) => {
           subj = subj[lenspath[index]]
         })
         mkeys = Object.keys(subj)
@@ -24,85 +27,83 @@
       }
     }
 
-    prototype._$map = function(fn,overlook) {
-      
-      let 
-        lookover = overlook === undefined 
-          ? mkeys : lob.hasOwnProperty(overlook) 
-          ? Object.keys(lob[overlook]) : []
-              
-      lookover.forEach((value,index) => {
+    prototype._$map = function(fn, overlook) {
+
+      let
+        lookover = overlook === undefined
+          ? mkeys : lob.hasOwnProperty(overlook)
+            ? Object.keys(lob[overlook]) : []
+
+      lookover.forEach((value, index) => {
         if (overlook !== undefined) {
-          lob[overlook][value] = fn(lob[overlook][value],value,index)
+          lob[overlook][value] = fn(lob[overlook][value], value, index)
         }
         if (overlook === undefined) {
           if (typeof lob[value] !== 'object') {
-            lob[value] = fn(lob[value],value,index)
+            lob[value] = fn(lob[value], value, index)
           }
         }
       })
 
     }
 
-    prototype._$scan = function(fn,overlook,acc) {
-      
-      let 
+    prototype._$scan = function(fn, overlook, acc) {
+
+      let
         init = arguments.length === 2
-          , lookover = overlook === '' 
-          ? mkeys : lob.hasOwnProperty(overlook) 
-          ? Object.keys(lob[overlook]) : []
-          
-        lookover.forEach((value) => {
-          if (overlook !== '') {
-            if (!init) {
-              acc = fn(acc,lob[overlook][value],value)
-              lob[overlook][value] = acc
+        , lookover = overlook === ''
+          ? mkeys : lob.hasOwnProperty(overlook)
+            ? Object.keys(lob[overlook]) : []
+
+      lookover.forEach((value) => {
+        if (overlook === '') {
+          if (typeof lob[value] !== 'object') {
+            if (init) {
+              init = false
+              acc = lob[value]
+              lob[value] = acc
             }
             else {
-              init = false
-              acc = lob[overlook][value]
-              lob[overlook][value] = acc
+              acc = fn(acc, lob[value], value)
+              lob[value] = acc
             }
           }
-          if (overlook === '') {
-            if (typeof lob[value] !== 'object') {
-              if (!init) {
-                acc = fn(acc,lob[value],value)
-                lob[value] = acc
-              }
-              else {
-                init = false
-                acc = lob[value]
-                lob[value] = acc
-              }
-            }
+        }
+        else {
+          if (init) {
+            init = false
+            acc = lob[overlook][value]
+            lob[overlook][value] = acc
           }
-        })
+          else {
+            acc = fn(acc, lob[overlook][value], value)
+            lob[overlook][value] = acc
+          }
+        }
+      })
 
-        return lob
+      return lob
 
-      }
+    }
 
-      prototype._$commit = function() {
-        mkeys.forEach((value) => {
-          rdx[value] = lob[value]
-        })
-      }
+    prototype._$commit = function() {
+      mkeys.forEach((value) => {
+        rdx[value] = lob[value]
+      })
+    }
 
-      prototype._$rdx = rdx
-     
-      this.lob = Object.create(prototype)
-      this.lob = Object.assign(this.lob,see(nGet(lthru),obj))
-      
-      return this.lob
-  
+    prototype._$rdx = rdx
+
+    this.lob = Object.create(prototype)
+    this.lob = Object.assign(this.lob, see(nGet(lthru), obj))
+
+    return this.lob
+
   }
 
   // window.l = Lob
   // return l
 
-  typeof module !== 'undefined' 
-  ? module.exports = Lob 
-  : window.lens = Lob
+  typeof module === 'undefined' ? window.lens = Lob : module.exports = Lob
 
 }())
--- stream.js
+++ stream.js
@@ -1,14 +1,15 @@
-;(function(){
+;(function() {
 
   function Stream(value) {
 
-    var dStreams = []
-    var dFns = []
+    var
+      dStreams = []
+      , dFns = []
 
     function stream(val) {
       if (val !== undefined) {
         value = val
-        dFns.forEach((fn,i) => {
+        dFns.forEach((fn, i) => {
           dStreams.length > 0 ? dStreams[i](fn(val)) : fn(val)
         })
       }
@@ -18,18 +19,19 @@
     stream.constructor = Stream
 
     stream.map = function(fn) {
-      let newStream = value !== undefined ? Stream(fn(value)) : Stream()
-        dStreams.push(newStream)
-        dFns.push(fn)
+      let
+        newStream = value === undefined ? Stream() : Stream(fn(value))
+      dStreams.push(newStream)
+      dFns.push(fn)
       return newStream
     }
 
     stream.scan = function(fn, acc) {
       let newStream = Stream(acc)
-        stream.map((v) => {
-          acc = fn(acc,v)
-          newStream(acc)
-        })
+      stream.map((v) => {
+        acc = fn(acc, v)
+        newStream(acc)
+      })
       return newStream
     }
 
@@ -41,7 +43,7 @@
     let
       newStream = Stream()
       , newArrStream = []
-    arrStream.forEach((v,i) => {
+    arrStream.forEach((v, i) => {
       v.map(function(value) {
         newArrStream[i] = value
         newStream(newArrStream)
@@ -53,8 +55,6 @@
 
   Stream.merge = merge
 
-  typeof module !== 'undefined' 
-  ? module.exports = Stream 
-  : window.stream = Stream
-  
+  typeof module === 'undefined' ? window.stream = Stream : module.exports = Stream
+
 }())
